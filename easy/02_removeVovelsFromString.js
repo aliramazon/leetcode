4f75bbf3882ff12694a6b683157aae7b325d0630
Safarali Ramazonov <aliramazon@Safaralis-MacBook-Pro.local>
2021-01-08 21:39:52 -0700

    Remove vovels from string

---
+++ easy/02_removeVovelsFromString.js
@@ -0,0 +1,32 @@
+/* Given a string s, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
+
+Example 1:
+
+Input: s = "leetcodeisacommunityforcoders"
+Output: "ltcdscmmntyfrcdrs"
+Example 2:
+
+Input: s = "aeiou"
+Output: ""
+ 
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of only lowercase English letters.
+
+*/
+
+const removeVovelsFromString = (string) => {
+   const vovels = "aeiou";
+   let result = "";
+   
+   for(let i = 0; i < string.length; i ++){
+       if(vovels.indexOf(string[i]) === -1){
+           result += string[i];
+       }
+   }
+   return result;
+}
+
+console.log(removeVovelsFromString("hello"));
