7b49bc7a3dcde6098f558d2254cd5baefdf1277f
mr.safarali@gmail.com <mr.safarali@gmail.com>
2021-01-30 23:45:11 -0700

    Clean up instruction

--- easy/20_balancedStrings.js
+++ easy/20_balancedStrings.js
@@ -9,16 +9,19 @@ Example 1:
 Input: s = "RLRRLLRLRL"
 Output: 4
 Explanation: s can be split into "RL", "RRLL", "RL", "RL", each substring contains same number of 'L' and 'R'.
+
 Example 2:
 
 Input: s = "RLLLLRRRLR"
 Output: 3
 Explanation: s can be split into "RL", "LLLRRR", "LR", each substring contains same number of 'L' and 'R'.
+
 Example 3:
 
 Input: s = "LLLLRRRR"
 Output: 1
 Explanation: s can be split into "LLLLRRRR".
+
 Example 4:
 
 Input: s = "RLRRRLLRLL"
