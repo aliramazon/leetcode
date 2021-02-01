2b44a5dd82fb87116fa2690de5bb097b229fb908
mr.safarali@gmail.com <mr.safarali@gmail.com>
2021-01-31 22:43:05 -0700

    Find number of consistent strings in array

---
+++ 22_countConsistentStrings.js
@@ -0,0 +1,62 @@
+/* You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+
+Return the number of consistent strings in the array words.
+
+ 
+
+Example 1:
+
+Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+Output: 2
+Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
+
+Example 2:
+
+Input: allowed = "abc", words = ["a","b","c","ab","ac","bc","abc"]
+Output: 7
+Explanation: All strings are consistent.
+
+Example 3:
+
+Input: allowed = "cad", words = ["cc","acd","b","ba","bac","bad","ac","d"]
+Output: 4
+Explanation: Strings "cc", "acd", "ac", and "d" are consistent.
+ 
+
+Constraints:
+
+1 <= words.length <= 104
+1 <= allowed.length <= 26
+1 <= words[i].length <= 10
+The characters in allowed are distinct.
+words[i] and allowed contain only lowercase English letters
+
+*/
+
+const countConsistentStrings = (allowed, words) => {
+    let count = 0;
+    const isConsistent = (allowed, word) => {
+        let set = new Set(allowed);
+
+        for (let i = 0; i < word.length; i++) {
+            if (!set.has(word[i])) return false;
+        }
+        return true;
+    };
+
+    for (let word of words) {
+        if (isConsistent(allowed, word)) count++;
+    }
+
+    return count;
+};
+
+const countConsistentStrings = (allowed, words) => {
+    let allowedSet = new Set(allowed);
+
+    const count = words.reduce((acc, word) => {
+        return [...word].every((char) => allowedSet.has(char)) ? acc + 1 : acc;
+    }, 0);
+
+    return count;
+};
