f4d567440c91400ff216d2bb81f064ea4a0c4106
mr.safarali@gmail.com <mr.safarali@gmail.com>
2021-12-19 14:13:22 -0700

    Reverse LinkedList recursively

--- easy/59_reverseLinkedList.js
+++ easy/59_reverseLinkedList.js
@@ -2,10 +2,13 @@
 Given the head of a singly linked list, reverse the list, and return the reversed list.
 
 Definition for singly-linked list.
- function ListNode(val, next) {
-     this.val = (val===undefined ? 0 : val)
-      this.next = (next===undefined ? null : next)
- }
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+1 -> 2 -> 3 -> 4 -> 5 -> 6
+1 -> 2 -> 3 -> 4 -> 5 <- 6
  */
 
 const reverseLinkedList = (head) => {
@@ -21,4 +24,10 @@ const reverseLinkedList = (head) => {
     return prev;
 };
 
-const reverseLinkedListRecursive = (head) => {};
+const reverseLinkedListRecursive = (head) => {
+    if (!head || !head.next) return head;
+    let preTail = reverseLinkedListRecursive(head.next);
+    head.next.next = head;
+    head.next = null;
+    return preTail;
+};
