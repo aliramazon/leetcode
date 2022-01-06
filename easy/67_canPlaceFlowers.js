9d1b25da753b31072e25e318b0994284062891cf
mr.safarali@gmail.com <mr.safarali@gmail.com>
2022-01-05 21:23:45 -0700

    Check if flowerbed capasity is enough

---
+++ easy/67_canPlaceFlowers.js
@@ -0,0 +1,37 @@
+/*
+You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+
+Example 1:
+Input: flowerbed = [1,0,0,0,1], n = 1
+Output: true
+
+Example 2:
+Input: flowerbed = [1,0,0,0,1], n = 2
+Output: false
+ 
+
+Constraints:
+1 <= flowerbed.length <= 2 * 104
+flowerbed[i] is 0 or 1.
+There are no two adjacent flowers in flowerbed.
+0 <= n <= flowerbed.length
+*/
+
+const canPlaceFlowers = (flowerbed, n) => {
+    let i = 0;
+    let counter = 0;
+
+    while (i < flowerbed.length) {
+        if (!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]) {
+            flowerbed[i] = 1;
+            counter++;
+            i += 2;
+        } else {
+            i++;
+        }
+        if (counter === n) return true;
+    }
+    return counter >= n;
+};
