43a371259da9d29de4f18279257d82c6a9769cb8
mr.safarali@gmail.com <mr.safarali@gmail.com>
2022-01-09 10:48:27 -0700

    Find first occurance of needle in haystack

---
+++ easy/73_firstIndexOfNeedle.js
@@ -0,0 +1,49 @@
+/* 
+Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+
+ 
+
+Example 1:
+Input: hays
+
+Example 2:
+Input: haystack = "aaaaa", needle = "bba"
+Output: -1
+
+Example 3:
+Input: haystack = "", needle = ""
+Output: 0
+ 
+
+Constraints:
+0 <= haystack.length, needle.length <= 5 * 104
+haystack and needle consist of only lower-case English characters.
+*/
+
+const firstIdxOfNeedle = (haystack, needle) => {
+    if (needle === '') return 0;
+    if (needle.length > haystack.length) return -1;
+
+    for (let i = 0; i < haystack.length; i++) {
+        let found = false;
+
+        if (haystack[i] === needle[0]) {
+            for (let j = 0; j < needle.length; j++) {
+                if (needle[j] === haystack[j + i]) {
+                    found = true;
+                } else {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) return i;
+        }
+    }
+    return -1;
+};
