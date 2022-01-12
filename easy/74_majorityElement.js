dcce25baad5bd9067a2d3f8522e449b27c80425f
mr.safarali@gmail.com <mr.safarali@gmail.com>
2022-01-11 18:41:36 -0700

    Find majority element using BoyerMoore algorithm

--- easy/74_majorityElement.js
+++ easy/74_majorityElement.js
@@ -37,3 +37,14 @@ const majorityElement = (nums) => {
 
     return majorElement;
 };
+
+const majorityElementBoyerMoore = (nums) => {
+    let count = 0;
+    let candidate = null;
+    for (let num of nums) {
+        if (count === 0) candidate = num;
+        count += num === candidate ? 1 : -1;
+    }
+
+    return candidate;
+};
