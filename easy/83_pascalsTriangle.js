698efe6acb905dac9b4e26690e91a9b6229d26db
mr.safarali@gmail.com <mr.safarali@gmail.com>
2022-01-23 19:52:20 -0700

    Generate Pascal's Triangle

---
+++ easy/83_pascalsTriangle.js
@@ -0,0 +1,48 @@
+/* 
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+In Pascal's triangle, each number is the sum of the two numbers
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+*/
+
+const pascalTriangleRecursive = (numRows) => {
+    let rows = [[1]];
+
+    const generate = (lastRow) => {
+        if (rows.length === numRows) return;
+        let temp = [1];
+        for (let i = 0; i < lastRow.length - 1; i++) {
+            temp.push(lastRow[i] + lastRow[i + 1]);
+        }
+        temp.push(1);
+        rows.push(temp);
+        generate(temp);
+    };
+    generate([1]);
+    return rows;
+};
+
+const pascalTriangle = (numberRows) => {
+    let rows = [[1]];
+    let nextRow = [1];
+
+    while (rows.length < numberRows) {
+        let lastRow = rows[rows.length - 1];
+        for (let i = 0; i < lastRow.length - 1; i++) {
+            nextRow.push(lastRow[i] + lastRow[i + 1]);
+        }
+        nextRow.push(1);
+        rows.push(nextRow);
+        nextRow = [1];
+    }
+    return rows;
+};
+
+console.log(pascalTriangleRecursive(5));
