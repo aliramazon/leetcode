d1e14172e51df545834da9fe6936fa6f0cbf2ec4
Safarali <mr.safarali@gmail.com>
2022-02-03 20:39:02 -0700

    Find if there is nearby duplicate

---
+++ easy/92_containsDuplicateII.js
@@ -0,0 +1,35 @@
+/* 
+Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+Example 1:
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+Example 2:
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+Example 3:
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+Constraints:
+1 <= nums.length <= 105
+-109 <= nums[i] <= 109
+0 <= k <= 105
+*/
+
+const containsNearbyDuplicate = (nums, k) => {
+    let map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (map.has(num) && Math.abs(map.get(num) - i) <= k) {
+            return true;
+        } else {
+            map.set(num, i);
+        }
+    }
+    return false;
+};
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
